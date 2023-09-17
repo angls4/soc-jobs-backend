e98b41808b36014b022f0e0368e1dc30ccd40b79
Tegar <tegardanardana991@gmail.com>
2023-09-17 20:16:19 +0700

    connecting repositories

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+// const route = require('./route');
+const session = require('express-session');
+// const passport = require('passport');
+
+// Catch body from request
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// app.use(session({
+//   resave: false,
+//   saveUninitialized: true,
+//   secret: 'secret'
+// }));
+
+// app.use(passport.initialize());
+// app.use(passport.session());
+
+// app.use("/", route);
+
+// Welcome page
+app.get('/', (req, res) => {
+  res.send('Welcome to Our Project'); // Frontend landing page
+});
+
+// Route not found
+app.use((req, res, next) => {
+  res.status(404).send('Route not found');
+});
+
+// Error handling
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).send(`Internal Server Error: ${err.message}`);
+});
+
+// Listen to port
+app.listen(port, () => {
+  console.log(`Projects listening on port ${port}`);
+});
